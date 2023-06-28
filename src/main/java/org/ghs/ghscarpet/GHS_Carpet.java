e4f900453441d461f2f38b7a207027c760b42b74
mc-doge666 <WANGYAOCING@outlook.com>
2023-06-28 11:05:18 +0800

    6Auto stash before merge of "master" and "origin/master"

--- src/main/java/org/ghs/ghscarpet/GHS_Carpet.java
+++ src/main/java/org/ghs/ghscarpet/GHS_Carpet.java
@@ -1,17 +1,4 @@
 package org.ghs.ghscarpet;
 
-import net.fabricmc.api.ModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-
-public class GHS_Carpet implements ModInitializer {
-    public static final String MOD_ID = "GHS-Carpet";
-    public static final Logger LOGGER = LoggerFactory.getLogger("GHS-Carpet");
-
-
-    @Override
-    public void onInitialize() {
-
-    }
+public class GHS_Carpet {
 }
