04ec25ebe5a56e8b2fc88923306ad2944dc0561f
mc-doge666 <WANGYAOCING@outlook.com>
2023-06-28 10:35:45 +0800

    修改main文件
    修改main文件，使json文件识别

--- src/main/java/org/ghs/ghscarpet/GHS_Carpet.java
+++ src/main/java/org/ghs/ghscarpet/GHS_Carpet.java
@@ -1,2 +1,17 @@
-public class GHS_Carpet {
+package org.ghs.ghscarpet;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class GHS_Carpet implements ModInitializer {
+    public static final String MOD_ID = "GHS-Carpet";
+    public static final Logger LOGGER = LoggerFactory.getLogger("GHS-Carpet");
+
+
+    @Override
+    public void onInitialize() {
+
+    }
 }
